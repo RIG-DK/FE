6e02324e8c8097902be1866168754c7d535f8d20
yannafaith <yannafaith97@gmail.com>
2019-06-12 18:28:40 -0700

    refactors editor for react hooks

--- dev/src/views/MyEditor.jsx
+++ dev/src/views/MyEditor.jsx
@@ -1,59 +1,46 @@
-import React from 'react';
+import React, {useState}from 'react';
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css';
 
-class MyEditor extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = { text: '' } // You can also pass a Quill Delta here
-      this.handleChange = this.handleChange.bind(this)
-    }
-  
-    handleChange(value) {
-      this.setState({ text: value })
-    }
-  
-    render() {
-      return (
-        <ReactQuill 
-            value={this.state.text}
-            onChange={this.handleChange}
-        />
-      )
-    }
-  }
-  
-/* 
-   * Quill modules to attach to editor
-   * See https://quilljs.com/docs/modules/ for complete options
-*/
+const MyEditor = () => {
 
-  MyEditor.modules = {
+    const [text, setText] = useState("");
+    const handleChange = (value) =>  {setText(value)};
+  
+    return (
+        <ReactQuill 
+            value={text}
+            onChange={handleChange}
+            modules={MyEditor.modules}
+            formats={MyEditor.formats}
+            placeholder="what's on your mind, Don?"
+        />
+    )
+}
+
+MyEditor.modules = {
     toolbar: [
-      [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-      [{size: []}],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, 
-       {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image', 'video'],
-      ['clean']
+        [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+        [{size: []}],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, 
+        {'indent': '-1'}, {'indent': '+1'}],
+        ['link', 'image', 'video'],
+        ['clean']
     ],
     clipboard: {
-      // toggle to add extra line breaks when pasting HTML:
-      matchVisual: false,
+        // toggle to add extra line breaks when pasting HTML:
+        matchVisual: true,
     }
-  }
-  /* 
-   * Quill editor formats
-   * See https://quilljs.com/docs/formats/
-   */
-  MyEditor.formats = [
-    'header', 'font', 'size',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image', 'video'
-  ]
+}
+
+MyEditor.formats = [
+'header', 'font', 'size',
+'bold', 'italic', 'underline', 'strike', 'blockquote',
+'list', 'bullet', 'indent',
+'link', 'image', 'video'
+]
   
 
 export default MyEditor;
